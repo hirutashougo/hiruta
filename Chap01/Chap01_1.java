a6fea3e44d730268cfe7647986cefa36607a5352
hirutashougo <ddt.shg.hiruta@gmail.com>
2024-04-03 18:07:26 +0900

    a

---
+++ Chap01/Chap01_1.java
@@ -0,0 +1,12 @@
+package Chap01;
+//クラスの宣言
+public class Chap01_1 {
+	//mainメソッドの宣言
+	public static void main(String[] args) {
+		//画面への出力を行う処理
+		System.out.println("初めてのJavaプログラム。")
+		//画面への出力を行う処理
+		System.out.println("画面に出力しています。")
+	}
+
+}
