8bb6de5c49fb81ee9b41c0e62386cafaf6bec545
hirutashougo <ddt.shg.hiruta@gmail.com>
2024-04-04 13:39:32 +0900

    修正

--- Chap01/Excersises/Exercise01_02.java
+++ Chap01/Excersises/Exercise01_02.java
@@ -1,7 +1,19 @@
 package Chap01.Excersises;
-//クラスの宣言
+/*
+ * クラス名:Exercise01_02
+ * 概要:名前の一文字ずつ改行して表示する
+ * 作成者:S.Hiruta
+ * 作成日:2024/04/01
+*/
 public class Exercise01_02 {
-	//mainメソッドの宣言
+	/*
+	*関数名:main
+	*概要:名前の一文字ずつ改行して表示する
+	*引数:なし
+	*戻り値:なし
+	*作成者:S.Hiruta
+	*作成日:2024/04/01
+	*/
 	public static void main(String[] args) {
 		//一文字ずつ改行して出力する処理
 		System.out.println("蛭\n田\n昌\n伍");
--- Chap01/Excersises/Exercise01_03.java
+++ Chap01/Excersises/Exercise01_03.java
@@ -1,7 +1,21 @@
 package Chap01.Excersises;
-//クラスの宣言
+/*
+ * クラス名:Exercise01_03
+ * 概要:名前の一文字ずつ改行し,
+ 		名前と苗字の間も改行してに表示する
+ * 作成者:S.Hiruta
+ * 作成日:2024/04/01
+ */
 public class Exercise01_03 {
-	//mainメソッドの宣言
+	/*
+	 *関数名:main
+	 *概要:名前の一文字ずつ改行し,
+		   名前と苗字の間も改行してに表示する
+	 *引数:なし
+	 *戻り値:なし
+	 *作成者:S.Hiruta
+	 *作成日:2024/04/01
+	 */
 	public static void main(String[] args) {
 		//一文字ずつ改行して出力する処理
 		System.out.println("蛭\n田\n\n昌\n伍");
