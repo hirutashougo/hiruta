9e22b9456f60cb17e5632a0f899d114f6f697aee
hirutashougo <ddt.shg.hiruta@gmail.com>
2024-04-04 18:50:46 +0900

    s

--- Chap03/Exercises/Exercise03_08.java
+++ Chap03/Exercises/Exercise03_08.java
@@ -1,25 +1,54 @@
 package Chap03.Exercises;
 
-//読み込んだ点数の成績を表示
-
+//Scannerの読み込み
 import java.util.Scanner;
 
+/*
+ * クラス名:Exercise03_08
+ * 読み込んだ点数の成績を表示
+ * 作成者:S.Hiruta
+ * 作成日:2024/04/02
+ */
 public class Exercise03_08 {
-
+	
+	/*
+	 *関数名:main
+	 *概要:読み込んだ点数の成績を表示
+	 *引数:なし
+	 *戻り値:なし
+	 *作成者:S.Hiruta
+	 *作成日:2024/04/02
+	 */
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
 		
-		System.out.print("成績を表示します。\n何点ですか:");  //点数の入力
-		int score = stdIn.nextInt();     //点数の読み込み
+		//Scannerクラスの変数の定義
+		Scanner standerdImput = new Scanner(System.in);
 		
-		if ( score >= 0 && score <= 59 )                       //不可
+		//点数の入力を促す
+		System.out.print("成績を表示します。\n何点ですか:");
+		//入力された点数を読み込む
+		int scoreNumber = standerdImput.nextInt();
+		
+		//入力された点数が0以上59以下の場合
+		if ( scoreNumber >= 0 && scoreNumber <= 59 ) {
+			//不可の出力を行う
 			System.out.println("不可");
-		else if ( score >= 60 && score <= 69 )                  //可
+		}
+		//入力された点数が60以上69以下の場合
+		else if ( scoreNumber >= 60 && scoreNumber <= 69 )  {
+			//可の出力を行う
 			System.out.println("可");
-		else if ( score >= 70 && score <= 79 )                 //良
+		}
+		//入力された点数が70以上79以下の場合
+		else if ( scoreNumber >= 70 && scoreNumber <= 79 ) {
+			//良の出力を行う
 			System.out.println("良");
-		else if ( score >= 80 && score <= 100 )                 //優
+		}
+		//入力された点数が80以上100以下の場合
+		else if ( scoreNumber >= 80 && scoreNumber <= 100 ) {
+			//優の出力を行う
 			System.out.println("優");
+		}
 
 	}
 
