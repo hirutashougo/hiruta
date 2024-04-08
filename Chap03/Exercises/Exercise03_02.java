185ad69b225cb17c62cdcaec87a859ef3896a03c
hirutashougo <ddt.shg.hiruta@gmail.com>
2024-04-08 17:01:32 +0900

    s

--- Chap03/Exercises/Exercise03_02.java
+++ Chap03/Exercises/Exercise03_02.java
@@ -26,29 +26,38 @@ public class Exercise03_02 {
 		
 		//整数値Aの入力及び読み込み(変数Aは教科書準拠 )
 		System.out.print("整数A:");
-		
 		//入力された整数値Aの読み込み
 		int integralNumberA = standardInput.nextInt();
 		
 		//整数値Bの入力及び読み込み(変数Bは教科書準拠 )
 		System.out.print("整数B:");
-		
 		//入力された整数値Bの読み込み
 		int integralNumberB = standardInput.nextInt();
 		
+		//整数A・Bが0ではない場合
+		if (integralNumberA != 0 && integralNumberB !=0) {
 		
-		//整数BとAの商の余りが0の場合
-		if ( integralNumberA % integralNumberB == 0 ) {
-			
-			//BはAの約数であることを伝える
-			System.out.println("BはAの約数です。");
-		}
+			//整数BとAの商の余りが0の場合
+			if ( integralNumberA % integralNumberB == 0 ) {
+
+				//BはAの約数であることを伝える
+				System.out.println("BはAの約数です。");
+			}
+
+			//整数BとAの商の余りが0以外の場合
+			else  {
+
+				//BはAの約数ではないことを伝える。
+				System.out.println("BはAの約数ではありません。");
+			}
+		} 
 		
-		//整数BとAの商の余りが0以外の場合
-		else  {
+		//整数A・Bが0である場合
+		else {
 			
-			//BはAの約数ではないことを伝える。
-			System.out.println("BはAの約数ではありません。");
+			//0は約数にならないことを伝える
+			System.out.print("0は約数になりません。");
+			
 		}
 
 	}
--- Chap04/Exercises/Exercise04_05.java
+++ Chap04/Exercises/Exercise04_05.java
@@ -1,19 +1,16 @@
 package Chap04.Exercises;
 
-//スキャナーのインポート
-import java.util.Scanner;
-
 /*
- * クラス名:Exercise04_04
- * 概要：List4-4のwhile文終了時にxの値が-1になることを確認する
+ * クラス名:Exercise04_05
+ * 概要：式の評価順が左→右であることを確認(前置増分演算子を用いて
  * 作成者:S.Hiruta
  * 作成日:2024/04/08
  */
 public class Exercise04_05 {
 	
-    /*
+	/*
      * 関数名：main
-     * 概要：List4-4のwhile文終了時にxの値が-1になることを確認する
+     * 概要：式の評価順が左→右であることを確認(前置増分演算子を用いて
      * 引数：なし
      * 戻り値：なし
      * 作成者：hiruta
@@ -22,41 +19,18 @@ public class Exercise04_05 {
     
     public static void main(String[] args) {
     	
-    	//Scannerクラスの変数の設定
-        Scanner standardInput = new Scanner(System.in);
-        
-        //カウントダウン開始の表示
-        System.out.println("カウントダウンします。");
-        
-        //変数xの初期状態を0で設定(変数xは教科書準拠)
-        int x = 0 ;
-        
-        /*変数xが0以下である限り、入力した正の整数から0までの
-         カウントダウンを表示し続ける処理*/
-        //do文の開始
-        do {
-        	
-        	//正の整数の入力を促す
-        	System.out.print("正の整数値：");
-        	//入力された正の整数を読み込む
-        	x = standardInput.nextInt();
-        
-        	//xが0以下である限りこの処理は繰り返す
-        } while (x <= 0 );
-        
-        //xが0以上
-        while (x >= 0) {
-        	
-        	//
-        	System.out.println(x);
-        	//
-        	x--;
-        	
-        }
-        
-        //while文終了後、変数xの値が-1で終わっている事を確認
-        System.out.println( "変数xの中身は" + x + "で終わっています。");
-
+    	//変数aを初期状態3で定義(変数aは教科書準拠)
+    	int a = 3;
+    	/*変数xの式を定義
+    	 (変数xは教科書準拠,後置増分演算子から前置増分演算子に変更)*/
+    	int x = (++a) * (2 + a);
+    	
+    	//変数aの値を表示
+    	System.out.println("a = " + a);
+    	//変数xの値を表示
+    	System.out.println("x = " + x);
     }
+    /*式の評価が左から右に行われているため、
+     変数xの値が4*(2+4)=24となっている*/
     
 }
